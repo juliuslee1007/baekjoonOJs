0d95b391acdbde563d0ee64b0303edd0dd7d0b07
juliuslee1007 <julius1007lee@gmail.com>
2026-05-30 09:29:41 +0900

    [level 1] Title: 폰켓몬, Time: 1.16 ms, Memory: 12.1 MB -BaekjoonHub

--- 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
+++ 프로그래머스/1/1845. 폰켓몬/폰켓몬.py
@@ -1,11 +1,3 @@
 def solution(nums):
-    count = {}
-    for p in nums:
-        if p in count:
-            count[p] += 1
-        else:
-            count.update({p : 1})
-    if len(count) > len(nums) // 2:
-         return len(nums) // 2
-    else:
-        return len(count)
+    answer = 0
+    return answer
